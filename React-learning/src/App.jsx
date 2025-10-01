90058f84cd399e9e483bf18bd373e28ce5b3ff98
CyberCrypter <your-cybercrypter-email@example.com>
2025-10-01 18:23:56 +0530

    passing props object

--- React-learning/src/App.jsx
+++ React-learning/src/App.jsx
@@ -1,7 +1,18 @@
 const Card = ({title}) => {
   return (
     <div>
-      <h1 className="text-blue-600">{title}</h1>
+      <h1 className="text-red-600">{title}</h1>
+    </div>
+  )
+}
+
+const Actors = ({actor}) => {
+  return (
+    <div>
+      <h1> <span className="text-blue-600">Actor name:</span> {actor[0].name}</h1>
+      <h1> <span className="text-blue-600">Age:</span> {actor[1].age}</h1>
+      <h1> <span className="text-blue-600">Character:</span> {actor[2].character}</h1>
+      <br />
     </div>
   )
 }
@@ -13,6 +24,9 @@ const App = () => {
         <Card title="Avenger"/>
         <Card title="Endgame"/>
         <Card title="Civil Wars"/>
+        <Actors actor={[{name: "Robert Downey Jr."}, {age: 20}, {character: "Ironman"}]} />
+        <Actors actor={[{name: "Chris Evans"}, {age: 22}, {character: "Captain America"}]} />
+        <Actors actor={[{name: "Chris Hemsworth"}, {age: 25}, {character: "Thor"}]} />
       </div>
     )
 }
